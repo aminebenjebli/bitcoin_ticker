6c404a1b1cce22553a6a900f65125593fd461491
aminebenjebli <mohamedamine.benjebli@esprit.tn>
2025-05-16 22:17:13 +0100

    Setup project/add model and initial screen

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:bitcoin_ticker/price_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+          primaryColor: Colors.lightBlue,
+          scaffoldBackgroundColor: Colors.white),
+      home: const PriceScreen(),
+    );
+  }
+}
